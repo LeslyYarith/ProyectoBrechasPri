9e47b512ff3d8a94860d4d676d3ae06b4d090987
Cristian Mendez <cgmendezme@gmail.com>
2025-08-31 16:37:34 -0500

    WIP antes de actualizar a Angular 20

--- proyecto_brechas1/src/app/app.config.ts
+++ proyecto_brechas1/src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),                      // ✅ HTTP listo
+    provideHttpClient(withFetch()),                      // ✅ HTTP listo
     provideRouter(routes),                    // ✅ Rutas
     provideZoneChangeDetection({ eventCoalescing: true }), // ✅ CD
     provideClientHydration(withEventReplay()) // ✅ Hydration SSR
